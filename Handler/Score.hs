b5798cd6ec8aa7ad8d7ef78d859f46066b425a90
Erlend Hamberg <ehamberg@gmail.com>
2013-02-06 21:45:31 +0100

    verify hash before adding score

--- Handler/Score.hs
+++ Handler/Score.hs
@@ -2,8 +2,18 @@
 module Handler.Score where
 
 import Import
-import Data.Aeson.Types
+import Data.Aeson.Types (Result (..))
 import Data.Text (pack)
+import Data.Time.Clock (UTCTime, getCurrentTime)
+import Data.Convertible (convert)
+import System.Posix.Types (EpochTime)
+import Data.Digest.Pure.SHA (Digest, SHA1State, hmacSha1)
+import Data.Int (Int64)
+import qualified Data.ByteString.Lazy.Char8 as B
+import Data.Text.Encoding (encodeUtf8)
+
+secret :: B.ByteString
+secret = B.pack "secret" -- :-)
 
 getScoreR :: Handler RepJson
 getScoreR = do
@@ -13,8 +23,25 @@ getScoreR = do
 putScoreR :: Handler RepJson
 putScoreR = do
   entry <- parseJsonBody
+  t <- liftIO getCurrentTime
   case entry of
        Error s   -> jsonToRepJson $ String (pack s)
-       Success v -> do
-         _ <- runDB $ insert (v :: CEScore)
-         jsonToRepJson $ String "ok"
+       Success v -> case validate v t of
+                         Left e  -> jsonToRepJson $ String e
+                         Right _ -> do
+                           _ <- runDB $ insert v
+                           jsonToRepJson $ String "success"
+
+validate :: CEScore -> UTCTime -> Either Text CEScore
+validate s@(CEScore name timestamp score hash) t = if validHash
+                                                      then Right s
+                                                      else Left "Invalid hash"
+  where validHash = hash == (pack . show) (scoreHash name (convert timestamp) score)
+
+scoreHash :: Text -> EpochTime -> Int64 -> Digest SHA1State
+scoreHash n t s = hmacSha1 secret scoredesc
+  where scoredesc = B.fromChunks [encodeUtf8 n]
+                      `B.append` B.pack "\v"
+                      `B.append` (B.pack .show) t
+                      `B.append` B.pack "\v"
+                      `B.append` (B.pack .show) s
